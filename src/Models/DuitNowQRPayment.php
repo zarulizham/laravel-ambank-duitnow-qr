1c58dac6c0fbbe3809e925ead6fb587605d7a6db
Zarul Zubir <zarul.izham@gmail.com>
2024-03-26 10:28:01 +0800

    Fix fillable and qr string on migration

--- src/Models/DuitNowQRPayment.php
+++ src/Models/DuitNowQRPayment.php
@@ -9,13 +9,26 @@ class DuitNowQRPayment extends Model
     protected $table = 'duitnow_qr_payments';
 
     protected $fillable = [
-        'reference_id',
-        'source_reference_number',
-        'transaction_status',
-        'amount',
-        'request_payload',
-        'response_payload',
+        'transaction_id',
+        'end_id',
+        'biz_id',
         'qr_string',
-        'qr_code',
+        'transaction_status',
+        'reason',
+        'sender_name',
+        'amount',
+        'currency',
+        'account_number',
+        'paid_at',
+        'source_of_fund',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'paid_at' => 'datetime',
     ];
 }
